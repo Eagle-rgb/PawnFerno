ecbdc592fc7676f6f58d2f73f170471174f25611
Eagle-rgb <leonphilipp2001@gmail.com>
2021-08-30 15:38:24 +0200

    Defined base types

--- PawnFerno/PawnFernoEngine/types.h
+++ PawnFerno/PawnFernoEngine/types.h
@@ -1,1 +1,39 @@
 #pragma once
+#include <cstdint>
+
+typedef uint64_t BitBoard;
+
+enum Squares {
+	a1, b1, c1, d1, e1, f1, g1, h1,
+	a2, b2, c2, d2, e2, f2, g2, h2,
+	a3, b3, c3, d3, e3, f3, g3, h3,
+	a4, b4, c4, d4, e4, f4, g4, h4,
+	a5, b5, c5, d5, e5, f5, g5, h5,
+	a6, b6, c6, d6, e6, f6, g6, h6,
+	a7, b7, c7, d7, e7, f7, g7, h7,
+	a8, b8, c8, d8, e8, f8, g8, h8
+};
+
+enum Ranks {
+	RANK1, RANK2, RANK3, RANK4, RANK5, RANK6, RANK7, RANK8
+};
+
+enum Files {
+	FILEA, FILEB, FILEC, FILED, FILEE, FILEF, FILEG, FILEH
+};
+
+enum PieceTypes {
+	PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
+};
+
+enum Directions {
+	NORTH = 8,
+	EAST = 1,
+	SOUTH = -NORTH,
+	WEST = -EAST,
+
+	SOUTHEAST = SOUTH + EAST,
+	SOUTHWEST = SOUTH + WEST,
+	NORTHWEST = NORTH + WEST,
+	NORTHEAST = NORTH + EAST
+};

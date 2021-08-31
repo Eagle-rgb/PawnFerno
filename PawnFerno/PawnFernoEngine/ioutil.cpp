76b82fd908537e92cd14856249112893fb4e98d1
arnegideon <arne.gideon@gmail.com>
2021-08-31 18:29:42 +0200

    Macht ihr mal

--- PawnFerno/PawnFernoEngine/ioutil.cpp
+++ PawnFerno/PawnFernoEngine/ioutil.cpp
@@ -1,18 +1,29 @@
 #include "ioutil.h"
+#include <string>
+
+string toBitString(BitBoard bb) {
+    string out = "";
+    for (int i=0; i < 64; i++){
+        out += to_string(bb & 1);
+        bb >>= 1;
+    }
+    return out;
+}
 
 void print(BitBoard bb){
-    for (int i=0; i < 64; i++){
-        cout << " " << (bb & 1);
-        bb >>= 1;
-        if (i % 8 == 7) {
-            cout << endl;
+    string bs = toBitString(bb);
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
+
         }
     }
-
 }
 
 int main() {
     BitBoardInit();
-    print(PAWN_CAPTURES[0][9]);
+    // print(BB_FILEA);
+
+    cout << toBitString(BB_FILEA) << endl;
+
     return 0;
 }
